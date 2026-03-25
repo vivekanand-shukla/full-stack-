2c127c62bb731214774e1390a272e63438f429d2
VIVEK SHUKLA <primevivek14@gmail.com>
2026-03-25 17:02:30 +0530

    done

--- Aiquiz/frontend/src/api.js
+++ Aiquiz/frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const generateQuiz = (topic) => {
-  return axios.post("http://localhost:5000/api/quiz/generate", {
+  return axios.post("https://full-stack-xpzh.vercel.app/api/quiz/generate", {
     topic
   });
 };
