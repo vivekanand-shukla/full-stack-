47dec1a1f015939d7c9a24fec5ae9786eccd3c31
VIVEK SHUKLA <primevivek14@gmail.com>
2026-03-25 16:51:06 +0530

    done

--- ful stack ai app/backend/routes/quizRoutes.js
+++ ful stack ai app/backend/routes/quizRoutes.js
@@ -10,7 +10,7 @@ router.post("/generate", async (req, res) => {
     const response = await axios.post(
       "https://openrouter.ai/api/v1/chat/completions",
       {
-      model:  Modele || "stepfun/step-3.5-flash:free",
+      model:   "stepfun/step-3.5-flash:free",
         messages: [
            
           {
